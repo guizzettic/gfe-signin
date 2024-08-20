2a439ae47a2efb5106cf828c38d49be3cb42c8b6
Chris Guizzetti <guizzettic@gmail.com>
2024-08-19 18:27:56 -0700

    adds error toast notification

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import AuthForm from "../components/AuthForm";
+import ErrorMessage from "../components/ErrorMessage";
 import useAuth from "../hooks/useAuth";
 import signinImage from "../img/sign-in.jpg";
 
@@ -8,6 +9,7 @@ export const SignIn: React.FC = () => {
   return (
     <div className="flex min-h-screen flex-col bg-gradient-to-b from-gray-50 to-gray-300">
       <div className="flex w-full grow flex-col items-center justify-center px-4 py-8 md:p-4">
+        <ErrorMessage errorType="You must agree to the Terms of Service to create an account." />
         <div className="flex w-full max-w-[343px] grow flex-col items-center justify-center rounded-lg bg-white shadow-lg md:max-w-[736px] xl:h-[736px] xl:max-w-[1408px] xl:flex-row">
           <div className="flex flex-col items-center justify-center gap-6 px-3 py-8 md:p-8 xl:w-1/2 xl:px-24 xl:py-8">
             <h1 className="self-start text-3xl font-semibold text-neutral-900 xl:pl-[60px]">
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import useAuth from "../hooks/useAuth";
 import AuthForm from "../components/AuthForm";
 import signupImage from "../img/sign-up.jpg";
+import ErrorMessage from "../components/ErrorMessage";
 
 const SignUp: React.FC = () => {
   const { signUp } = useAuth();
 
   return (
     <div className="flex min-h-screen flex-col bg-gradient-to-b from-gray-50 to-gray-300">
+      <ErrorMessage errorType="You must agree to the Terms of Service to create an account." />
       <div className="flex w-full grow flex-col items-center justify-center px-4 py-8 md:p-4">
         <div className="flex w-full max-w-[343px] grow flex-col items-center justify-center rounded-lg bg-white shadow-lg md:max-w-[736px] xl:h-[736px] xl:max-w-[1408px] xl:flex-row">
           <div className="flex flex-col items-center justify-center gap-6 px-3 py-8 md:p-8 xl:w-1/2 xl:px-24 xl:py-8">
