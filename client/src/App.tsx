0bda20d2c2e549c2d2f6d0a7d2656482d85f03fe
Chris Guizzetti <guizzettic@gmail.com>
2024-08-20 12:35:35 -0700

    lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,25 @@
+import { lazy, Suspense } from "react";
 import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import AuthSuccess from "./pages/AuthSuccess";
+
+const SignIn = lazy(() => import("./pages/SignIn"));
+const SignUp = lazy(() => import("./pages/SignUp"));
+const AuthSuccess = lazy(() => import("./pages/AuthSuccess"));
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/signin" Component={SignIn} />
-          <Route path="/signup" Component={SignUp} />
-          <Route path="/dashboard" Component={AuthSuccess} />
-          <Route path="/" Component={SignIn} />
-        </Routes>
-      </Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <Routes>
+            <Route path="/signin" Component={SignIn} />
+            <Route path="/signup" Component={SignUp} />
+            <Route path="/dashboard" Component={AuthSuccess} />
+            <Route path="/" Component={SignIn} />
+          </Routes>
+        </Router>
+      </Suspense>
     </AuthProvider>
   );
 };
