1d0b47539e33879855ac60170aae692cab960701
Chris Guizzetti <guizzettic@gmail.com>
2024-08-23 14:45:11 -0700

    small styling

--- client/src/Pricing/components/Subscription.tsx
+++ client/src/Pricing/components/Subscription.tsx
@@ -20,7 +20,7 @@ const Subscription: React.FC<SubscriptionProps> = ({
   } = plan;
 
   return (
-    <div className="flex flex-1 flex-col justify-between gap-2 self-stretch rounded-lg border-[0.5px] border-solid border-neutral-200 md:min-h-[532px]">
+    <div className="flex flex-1 flex-col justify-between gap-2 self-stretch rounded-lg border-[1px] border-solid border-neutral-200 md:min-h-[532px]">
       {popular && (
         <div className="flex items-center justify-center rounded-tl-md rounded-tr-md bg-indigo-50 px-2 py-4">
           <h1 className="text-center text-xl font-bold text-indigo-700">
@@ -29,14 +29,14 @@ const Subscription: React.FC<SubscriptionProps> = ({
         </div>
       )}
 
-      <div className="flex flex-col gap-2 p-4 md:px-8 lg:px-4">
+      <div className="flex flex-col gap-2 p-4 md:p-8 lg:p-6">
         <h2 className="text-2xl font-semibold text-neutral-900">{name}</h2>
         <span className="text-base font-normal text-neutral-600">
           {planDescription}
         </span>
       </div>
 
-      <div className="flex flex-col gap-2 p-4 md:px-8 lg:px-4">
+      <div className="flex flex-col gap-2 p-4 md:px-8 lg:p-6">
         <div>
           <span className="text-base font-normal text-indigo-700">
             <span className="text-5xl font-semibold text-indigo-700">
@@ -48,7 +48,7 @@ const Subscription: React.FC<SubscriptionProps> = ({
         <span className="text-base font-normal text-neutral-600">{`${monthlyPricing ? monthlyPrice[1] : annualPrice[1]}`}</span>
       </div>
 
-      <ul className="flex flex-col gap-5 self-stretch p-4 md:min-h-44 md:px-8 lg:min-h-[300px] lg:px-4">
+      <ul className="flex flex-col gap-5 self-stretch p-4 md:min-h-44 md:px-8 lg:min-h-[300px] lg:p-6">
         {planBenefit.map((benefit: string) => (
           <div className="flex items-center gap-3 self-stretch">
             <img
@@ -66,7 +66,7 @@ const Subscription: React.FC<SubscriptionProps> = ({
       </ul>
 
       {/* flex justify-center items-center gap-1.5 self-stretch bg-white px-5 py-3 rounded border-[0.5px] border-solid border-neutral-200 */}
-      <div className="flex flex-col justify-center gap-1.5 self-stretch px-4 pb-4 md:px-8 md:pb-6 lg:px-4">
+      <div className="flex flex-col justify-center gap-1.5 self-stretch px-4 pb-4 md:px-8 md:pb-6 lg:p-6">
         <button
           className={`rounded border-[0.5px] border-solid px-5 py-3 text-base font-medium shadow-lg ${popular ? "bg-indigo-700 text-white" : "bg-white text-neutral-900"}`}
         >
--- client/src/Pricing/pages/Pricing.tsx
+++ client/src/Pricing/pages/Pricing.tsx
@@ -9,15 +9,12 @@ const Pricing: React.FC = () => {
   return (
     <div className="flex h-screen w-full flex-col bg-gradient-to-b from-gray-50 to-gray-300">
       <div className="m-4 flex grow flex-col items-center overflow-y-auto rounded-md bg-white pt-12 shadow-md lg:pt-20">
-        {/* container */}
-        <div className="flex w-80 flex-col gap-12 px-2 md:w-[704px] lg:w-[1216px] lg:gap-16 lg:px-10">
-          {/* pricing content */}
+        <div className="flex w-80 flex-col gap-12 px-2 md:w-[704px] lg:w-[1216px] lg:gap-16">
           <PricingHeader
             setMonthlyPricing={setMonthlyPricing}
             monthlyPricing={monthlyPricing}
           />
 
-          {/* subscription tier */}
           <div className="flex flex-col items-center justify-center gap-8 self-stretch lg:flex-row">
             {PLAN_DETAILS.map((plan) => (
               <Subscription
