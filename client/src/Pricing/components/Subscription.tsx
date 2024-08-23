7e3d44bc64290ddd4148e9bde6e221ebfa59a7cd
Chris Guizzetti <guizzettic@gmail.com>
2024-08-23 13:27:52 -0700

    tablet responsiveness for subscriptions

--- client/src/Pricing/components/Subscription.tsx
+++ client/src/Pricing/components/Subscription.tsx
@@ -9,7 +9,7 @@ const Subscription: React.FC<SubscriptionProps> = ({ plan }) => {
   const { name, planDescription, price, popular, planBenefit } = plan;
 
   return (
-    <div className="flex w-[319px] flex-col justify-between gap-2 self-stretch rounded-lg border-[0.5px] border-solid border-neutral-200 bg-gray-400">
+    <div className="flex flex-col justify-between gap-2 self-stretch rounded-lg border-[0.5px] border-solid border-neutral-200 bg-gray-400 md:min-h-[532px]">
       {popular && (
         <div className="flex items-center justify-center rounded-tl-md rounded-tr-md bg-indigo-50 px-2 py-4">
           <h1 className="text-center text-xl font-bold text-indigo-700">
@@ -18,14 +18,14 @@ const Subscription: React.FC<SubscriptionProps> = ({ plan }) => {
         </div>
       )}
 
-      <div className="flex flex-col gap-2 p-4">
+      <div className="flex flex-col gap-2 bg-yellow-200 p-4 md:px-8">
         <h2 className="text-2xl font-semibold text-neutral-900">{name}</h2>
         <span className="text-base font-normal text-neutral-600">
           {planDescription}
         </span>
       </div>
 
-      <div className="flex flex-col gap-2 p-4">
+      <div className="flex flex-col gap-2 p-4 md:px-8">
         <div>
           <span className="text-base font-normal text-indigo-700">
             <span className="text-5xl font-semibold text-indigo-700">
@@ -37,7 +37,7 @@ const Subscription: React.FC<SubscriptionProps> = ({ plan }) => {
         <span className="text-base font-normal text-neutral-600">{`${popular ? "Prices in USD" : "Billed monthly"}`}</span>
       </div>
 
-      <ul className="flex flex-col gap-5 self-stretch p-4">
+      <ul className="flex flex-col gap-5 self-stretch p-4 md:min-h-44 md:px-8">
         {planBenefit.map((benefit) => (
           <div className="flex items-center gap-3 self-stretch">
             <img
@@ -54,7 +54,7 @@ const Subscription: React.FC<SubscriptionProps> = ({ plan }) => {
         ))}
       </ul>
 
-      <div className="flex flex-col justify-center gap-2 self-stretch px-4 pb-4">
+      <div className="flex flex-col justify-center gap-2 self-stretch px-4 pb-4 md:px-8 md:pb-6">
         <button
           className={`rounded px-5 py-3 text-base font-medium ${popular ? "bg-indigo-700 text-white" : "bg-white text-neutral-900"}`}
         >
