ca0cd1c8a4627feac21dc78eb07c8b879742f149
Chris Guizzetti <guizzettic@gmail.com>
2024-08-23 12:48:39 -0700

    adds PricingHeader component

---
+++ client/src/Pricing/components/PricingHeader.tsx
@@ -0,0 +1,40 @@
+interface PricingHeader {
+  monthlyPricing: boolean;
+  setMonthlyPricing: (option: boolean) => void;
+}
+
+const PricingHeader: React.FC<PricingHeader> = ({
+  setMonthlyPricing,
+  monthlyPricing,
+}) => {
+  return (
+    <div className="mt-0 flex h-72 flex-col items-center justify-between bg-blue-200">
+      <span className="text-center text-base font-semibold text-indigo-700">
+        Pricing Tiers
+      </span>
+      <h1 className="text-3xl font-semibold text-neutral-900">
+        Fit for all your needs
+      </h1>
+      <span className="text-center text-lg font-normal text-neutral-600">
+        Pick the plan that suits you today and step up as your demands grow -
+        our flexible options have your journey mapped out.
+      </span>
+      <div className="flex items-center self-stretch rounded">
+        <button
+          onClick={() => setMonthlyPricing(true)}
+          className={`flex grow items-center justify-center rounded bg-white text-base font-medium ${monthlyPricing ? `gap-1.5 border-[0.5px] border-solid border-neutral-200 px-4 py-2.5 text-neutral-900` : `gap-2 rounded-br rounded-tr text-neutral-600`}`}
+        >
+          Monthly
+        </button>
+        <button
+          onClick={() => setMonthlyPricing(false)}
+          className={`flex grow items-center justify-center rounded bg-white text-base font-medium ${!monthlyPricing ? `gap-1.5 border-[0.5px] border-solid border-neutral-200 px-4 py-2.5 text-neutral-900` : `gap-2 rounded-br rounded-tr text-neutral-600`}`}
+        >
+          Annually
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PricingHeader;
