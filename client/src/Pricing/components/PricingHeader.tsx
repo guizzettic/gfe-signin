d22a2320a666d0b16e4f20469199e345d86e5a06
Chris Guizzetti <guizzettic@gmail.com>
2024-08-23 13:07:50 -0700

    responsive price header

--- client/src/Pricing/components/PricingHeader.tsx
+++ client/src/Pricing/components/PricingHeader.tsx
@@ -8,18 +8,18 @@ const PricingHeader: React.FC<PricingHeader> = ({
   monthlyPricing,
 }) => {
   return (
-    <div className="mt-0 flex h-72 flex-col items-center justify-between bg-blue-200">
+    <div className="flex h-72 flex-col items-center justify-between bg-blue-200 md:px-8 lg:px-40">
       <span className="text-center text-base font-semibold text-indigo-700">
         Pricing Tiers
       </span>
-      <h1 className="text-3xl font-semibold text-neutral-900">
+      <h1 className="text-3xl font-semibold text-neutral-900 md:text-5xl">
         Fit for all your needs
       </h1>
-      <span className="text-center text-lg font-normal text-neutral-600">
+      <span className="text-center text-lg font-normal text-neutral-600 md:text-xl">
         Pick the plan that suits you today and step up as your demands grow -
         our flexible options have your journey mapped out.
       </span>
-      <div className="flex items-center self-stretch rounded">
+      <div className="flex w-80 items-center rounded">
         <button
           onClick={() => setMonthlyPricing(true)}
           className={`flex grow items-center justify-center rounded bg-white text-base font-medium ${monthlyPricing ? `gap-1.5 border-[0.5px] border-solid border-neutral-200 px-4 py-2.5 text-neutral-900` : `gap-2 rounded-br rounded-tr text-neutral-600`}`}
--- client/src/Pricing/pages/Pricing.tsx
+++ client/src/Pricing/pages/Pricing.tsx
@@ -8,9 +8,9 @@ const Pricing: React.FC = () => {
 
   return (
     <div className="flex h-screen w-full flex-col bg-gradient-to-b from-gray-50 to-gray-300">
-      <div className="m-4 flex grow flex-col items-center overflow-y-auto rounded-md bg-white pt-12 shadow-md">
+      <div className="m-4 flex grow flex-col items-center overflow-y-auto rounded-md bg-white pt-12 shadow-md lg:pt-20">
         {/* container */}
-        <div className="flex w-80 flex-col gap-12 bg-red-200">
+        <div className="flex w-80 flex-col gap-12 bg-red-200 md:w-[704px] lg:w-[1216px] lg:gap-16">
           {/* pricing content */}
           <PricingHeader
             setMonthlyPricing={setMonthlyPricing}
