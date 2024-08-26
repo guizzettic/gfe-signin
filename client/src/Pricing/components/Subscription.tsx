67aed70ad56222f6bacbd22878ac2afd8ceb0ff6
Chris Guizzetti <guizzettic@gmail.com>
2024-08-26 11:12:33 -0700

    changing height of subscription

--- client/src/Pricing/components/Subscription.tsx
+++ client/src/Pricing/components/Subscription.tsx
@@ -20,10 +20,10 @@ const Subscription: React.FC<SubscriptionProps> = ({ plan }) => {
 
   return (
     <div
-      className={`flex flex-1 flex-col justify-between gap-2 self-stretch rounded-lg border-[1px] border-solid border-neutral-200 md:min-h-[532px] ${popular ? "border-indigo-700 shadow-md" : ""}`}
+      className={`flex h-[644px] flex-1 flex-col justify-around self-stretch rounded-lg border-[1px] border-solid border-neutral-200 md:h-[612px] md:min-h-[532px] lg:h-[676px] ${popular ? "border-indigo-700 shadow-md" : ""} `}
     >
       {popular && (
-        <div className="flex items-center justify-center rounded-tl-md rounded-tr-md bg-indigo-50 px-2 py-4">
+        <div className="flex items-center justify-around rounded-tl-md rounded-tr-md bg-indigo-50 px-2 py-4">
           <h1 className="text-center text-xl font-bold text-indigo-700">
             Most Popular
           </h1>
@@ -51,7 +51,8 @@ const Subscription: React.FC<SubscriptionProps> = ({ plan }) => {
         <span className="text-base font-normal text-neutral-600">{`${monthlyPricing ? monthlyPrice[1] : annualPrice[1]}`}</span>
       </div>
 
-      <ul className="flex flex-col gap-5 self-stretch p-4 md:min-h-44 md:px-8 lg:min-h-[300px] lg:p-6">
+      <ul className="flex flex-col gap-3 self-stretch p-4 md:px-8 lg:p-6">
+        {/* <ul className="flex flex-col gap-5 self-stretch p-4 md:min-h-44 md:px-8 lg:min-h-[300px] lg:p-6"> */}
         {planBenefit.map((benefit: string) => (
           <div key={benefit} className="flex items-center gap-3 self-stretch">
             <img
@@ -65,7 +66,7 @@ const Subscription: React.FC<SubscriptionProps> = ({ plan }) => {
         ))}
       </ul>
 
-      <div className="flex flex-col justify-center gap-1.5 self-stretch px-4 pb-4 md:px-8 md:pb-6 lg:p-6">
+      <div className="relative bottom-1 flex flex-col justify-center self-stretch px-4 pb-4 md:px-8 md:pb-6 lg:p-6">
         <button
           className={`rounded border-[0.5px] border-solid px-5 py-3 text-base font-medium shadow-lg ${popular ? "bg-indigo-700 text-white" : "bg-white text-neutral-900"}`}
         >
