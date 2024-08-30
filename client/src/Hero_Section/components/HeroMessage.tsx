f78fe0b85ebe8c583c7dd49e652172c1e7443307
Chris Guizzetti <guizzettic@gmail.com>
2024-08-30 12:09:07 -0700

    accessibility for HeroMessage component

--- client/src/Hero_Section/components/HeroMessage.tsx
+++ client/src/Hero_Section/components/HeroMessage.tsx
@@ -11,10 +11,18 @@ const HeroMessage: React.FC = () => {
         </span>
       </div>
       <div className="flex gap-4 self-stretch md:w-[458px] md:gap-8 lg:w-[383px]">
-        <button className="flex grow items-center justify-center gap-1.5 rounded border-[0.5px] border-solid border-neutral-200 bg-white px-5 py-3 text-neutral-900 md:px-6 md:py-4 md:text-lg">
+        <button
+          aria-label="learn more button"
+          role="button"
+          className="flex grow items-center justify-center gap-1.5 rounded border-[0.5px] border-solid border-neutral-200 bg-white px-5 py-3 text-neutral-900 md:px-6 md:py-4 md:text-lg"
+        >
           Learn More
         </button>
-        <button className="flex grow items-center justify-center gap-1.5 rounded border-[0.5px] border-solid border-neutral-200 bg-indigo-700 px-5 py-3 text-white md:px-6 md:py-4 md:text-lg">
+        <button
+          aria-label="see pricing options"
+          role="button"
+          className="flex grow items-center justify-center gap-1.5 rounded border-[0.5px] border-solid border-neutral-200 bg-indigo-700 px-5 py-3 text-white md:px-6 md:py-4 md:text-lg"
+        >
           See pricing
         </button>
       </div>
