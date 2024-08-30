afc65f20731390543722d3ee46c8e4fcd35630ed
Chris Guizzetti <guizzettic@gmail.com>
2024-08-30 12:03:56 -0700

    fixing styling on laptop

--- client/src/Hero_Section/components/HeroMessage.tsx
+++ client/src/Hero_Section/components/HeroMessage.tsx
@@ -1,14 +1,14 @@
 const HeroMessage: React.FC = () => {
   return (
-    <div className="flex h-[260px] flex-col justify-center gap-6">
-      <h1 className="text-4xl font-semibold text-neutral-900 md:text-5xl">
+    <div className="flex h-[260px] flex-col justify-center gap-6 lg:gap-16">
+      <h1 className="text-4xl font-semibold text-neutral-900 md:text-5xl lg:text-6xl">
         Well crafted abstract images
       </h1>
       <span className="text-lg font-normal text-neutral-600 md:text-xl">
         High quality abstract images for your projects, wallpaper and
         presentations.
       </span>
-      <div className="flex gap-4 self-stretch md:w-[458px] md:gap-8">
+      <div className="flex gap-4 self-stretch md:w-[458px] md:gap-8 lg:w-[383px]">
         <button className="flex grow items-center justify-center gap-1.5 rounded border-[0.5px] border-solid border-neutral-200 bg-white px-5 py-3 text-neutral-900 md:px-6 md:py-4 md:text-lg">
           Learn More
         </button>
--- client/src/Hero_Section/pages/HeroSection.tsx
+++ client/src/Hero_Section/pages/HeroSection.tsx
@@ -3,13 +3,13 @@ import HeroMessage from "../components/HeroMessage";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="m-4 flex h-[780px] grow flex-col items-center justify-center rounded bg-white md:h-[992px]">
-      <div className="flex h-[668px] flex-col items-center justify-around gap-12 rounded px-3 py-12 md:h-[938px]">
+    <div className="m-4 flex h-[780px] grow flex-col items-center justify-center rounded bg-white md:h-[992px] lg:h-[736px]">
+      <div className="flex h-[668px] flex-col items-center justify-around gap-12 rounded px-3 py-12 md:h-[938px] lg:h-[718px] lg:w-[1408px] lg:flex-row lg:p-24">
         <HeroMessage />
         <img
           src={prism}
           aria-label="prism-photo"
-          className="flex items-center justify-center self-stretch rounded-lg object-cover py-[32.12166976928711px]"
+          className="flex grow items-center justify-center self-stretch rounded-lg object-cover py-[32.12166976928711px] lg:h-[526px] lg:w-[696px]"
         />
       </div>
     </div>
