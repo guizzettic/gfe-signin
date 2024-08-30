9f7a0c98b71296a864030190b2ed1b1f84dcc824
Chris Guizzetti <guizzettic@gmail.com>
2024-08-30 11:19:58 -0700

    fixed styling on tablet

--- client/src/Hero_Section/components/HeroMessage.tsx
+++ client/src/Hero_Section/components/HeroMessage.tsx
@@ -1,18 +1,18 @@
 const HeroMessage: React.FC = () => {
   return (
     <div className="flex h-[260px] flex-col justify-center gap-6">
-      <h1 className="text-4xl font-semibold text-neutral-900">
+      <h1 className="text-4xl font-semibold text-neutral-900 md:text-5xl">
         Well crafted abstract images
       </h1>
-      <span className="text-lg font-normal text-neutral-600">
+      <span className="text-lg font-normal text-neutral-600 md:text-xl">
         High quality abstract images for your projects, wallpaper and
         presentations.
       </span>
-      <div className="flex gap-4 self-stretch">
-        <button className="flex grow items-center justify-center gap-1.5 rounded border-[0.5px] border-solid border-neutral-200 bg-white px-5 py-3">
+      <div className="flex gap-4 self-stretch md:w-[458px] md:gap-8">
+        <button className="flex grow items-center justify-center gap-1.5 rounded border-[0.5px] border-solid border-neutral-200 bg-white px-5 py-3 text-neutral-900 md:px-6 md:py-4 md:text-lg">
           Learn More
         </button>
-        <button className="gap-1.5 rounded bg-indigo-700 px-5 py-3 text-base font-medium text-white">
+        <button className="flex grow items-center justify-center gap-1.5 rounded border-[0.5px] border-solid border-neutral-200 bg-indigo-700 px-5 py-3 text-white md:px-6 md:py-4 md:text-lg">
           See pricing
         </button>
       </div>
--- client/src/Hero_Section/pages/HeroSection.tsx
+++ client/src/Hero_Section/pages/HeroSection.tsx
@@ -3,8 +3,8 @@ import HeroMessage from "../components/HeroMessage";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="m-4 flex h-[780px] grow flex-col items-center justify-center rounded bg-white">
-      <div className="flex h-[668px] flex-col items-center justify-around gap-12 rounded px-3 py-12">
+    <div className="m-4 flex h-[780px] grow flex-col items-center justify-center rounded bg-white md:h-[992px]">
+      <div className="flex h-[668px] flex-col items-center justify-around gap-12 rounded px-3 py-12 md:h-[938px]">
         <HeroMessage />
         <img
           src={prism}
