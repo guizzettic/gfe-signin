15dd6b126430bea1742e9c2e85ad5d9997bc487d
Chris Guizzetti <guizzettic@gmail.com>
2024-08-30 12:07:14 -0700

    fixing styling on tablet

--- client/src/Hero_Section/components/HeroMessage.tsx
+++ client/src/Hero_Section/components/HeroMessage.tsx
@@ -1,13 +1,15 @@
 const HeroMessage: React.FC = () => {
   return (
-    <div className="flex h-[260px] flex-col justify-center gap-6 lg:gap-16">
-      <h1 className="text-4xl font-semibold text-neutral-900 md:text-5xl lg:text-6xl">
-        Well crafted abstract images
-      </h1>
-      <span className="text-lg font-normal text-neutral-600 md:text-xl">
-        High quality abstract images for your projects, wallpaper and
-        presentations.
-      </span>
+    <div className="flex h-[260px] flex-col justify-center gap-6 bg-black md:h-[252px] md:gap-16">
+      <div className="flex flex-col justify-center gap-6 self-stretch">
+        <h1 className="text-4xl font-semibold text-neutral-900 md:text-5xl lg:text-6xl">
+          Well crafted abstract images
+        </h1>
+        <span className="text-lg font-normal text-neutral-600 md:text-xl">
+          High quality abstract images for your projects, wallpaper and
+          presentations.
+        </span>
+      </div>
       <div className="flex gap-4 self-stretch md:w-[458px] md:gap-8 lg:w-[383px]">
         <button className="flex grow items-center justify-center gap-1.5 rounded border-[0.5px] border-solid border-neutral-200 bg-white px-5 py-3 text-neutral-900 md:px-6 md:py-4 md:text-lg">
           Learn More
