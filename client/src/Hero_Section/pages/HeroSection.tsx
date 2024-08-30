0168003c36633a5e2c144d9597db35b9727aa7a4
Chris Guizzetti <guizzettic@gmail.com>
2024-08-30 11:11:58 -0700

    fixed bg on mobile

--- client/src/Hero_Section/pages/HeroSection.tsx
+++ client/src/Hero_Section/pages/HeroSection.tsx
@@ -3,8 +3,8 @@ import HeroMessage from "../components/HeroMessage";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="m-4 flex h-[780px] grow flex-col items-center justify-center rounded">
-      <div className="flex h-[668px] flex-col items-center justify-around gap-12 bg-red-400 px-3 py-12">
+    <div className="m-4 flex h-[780px] grow flex-col items-center justify-center rounded bg-white">
+      <div className="flex h-[668px] flex-col items-center justify-around gap-12 rounded px-3 py-12">
         <HeroMessage />
         <img
           src={prism}
